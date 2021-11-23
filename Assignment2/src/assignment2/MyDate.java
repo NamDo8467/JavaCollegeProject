56b67c298203a2ed7bf8f4429e18446f756925a9
NamDo8467 <huynhnamdo@gmail.com>
2021-11-23 19:43:48 +0700

    test
    test

---
+++ Assignment2/src/assignment2/MyDate.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+/**
+ *
+ * @author huynh
+ */
+public class MyDate {
+        private int day;
+        private int month;
+        private int year;
+}
