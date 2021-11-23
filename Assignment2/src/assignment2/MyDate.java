3da83496f1fe61c68b787c1a7eb9b0da13811f97
NamDo8467 <huynhnamdo@gmail.com>
2021-11-23 19:58:04 +0700

    Update MyDate.java
    Complete the MyDate class

--- Assignment2/src/assignment2/MyDate.java
+++ Assignment2/src/assignment2/MyDate.java
@@ -13,4 +13,41 @@ public class MyDate {
         private int day;
         private int month;
         private int year;
+        
+        public MyDate(int day, int month, int year){
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+        
+        public int getDay(){
+            return day;
+        }
+        
+        public void setDay(int day){
+            this.day = day;
+        }
+        public String getMonthShortForm(){
+            return "H";
+        }
+        
+        public String getMonthLongForm(){
+            return "h";
+        }
+        public void setMonth(int month){
+            this.month = month;
+        }
+        public int getYear(){
+            return year;
+        }
+        public void setYear(int year){
+            this.year = year;
+        }
+        
+        public String toString(){
+            String s = "Day: " + day + ", Month: " + month + ", Year: " + year;
+            return s;
+        }
+            
+        
 }
