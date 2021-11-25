acfc4e5f3a5b2e15d3d10473bbc0b9199a0e8f3f
NamDo8467 <huynhnamdo@gmail.com>
2021-11-25 09:03:14 +0700

    Complete Address class
    Address class is finished based what the question asks. Maybe there will be additional functions.

--- Assignment2/src/assignment2/Address.java
+++ Assignment2/src/assignment2/Address.java
@@ -10,5 +10,28 @@ package assignment2;
  * @author huynh
  */
 public class Address {
+    public  String streetInfo1;
+    public String streetInfo2;
+    public String city;
+    public String postalCode;
+    public String country;
     
+    public Address(String streetInfo1, String streetInfo2, String city, String postalCode, String country){
+        this.streetInfo1 = streetInfo1;
+        this.streetInfo2 = streetInfo2;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.country = country;
+                
+    }
+    
+    @Override
+    public String toString(){
+        String s = "Street Information 1: " + streetInfo1 + "\n";
+        s += "Street Information 2: " + streetInfo2 + "\n";
+        s += "City: " + city + "\n";
+        s += "Postal Code: " + postalCode + "\n";
+        s += "Country: " + country + "\n";
+        return s;
+    }
 }
