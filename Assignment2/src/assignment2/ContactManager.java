3200431351c592cff43cc53137bee5f6088580c9
NamDo8467 <huynhnamdo@gmail.com>
2021-11-26 12:34:45 +0700

    Added searchByCity and searchByFirstNameAndLastName functions

---
+++ Assignment2/src/assignment2/ContactManager.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package assignment2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author huynh
+ */
+public class ContactManager {
+    private ArrayList<Contact> contactList;
+    
+    public ContactManager(){
+        contactList = new ArrayList<Contact>();
+    }
+
+    public void addContact(Contact contact){
+        contactList.add(contact);
+    }
+    
+    public void deleteContact(Contact contact){
+        try {
+            contactList.remove(contact);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+    }
+    
+    public Contact getContactAtIndex(int index){
+        return contactList.get(index);
+    }
+    
+    public int getIndexOfContact(Contact contact){
+        return contactList.indexOf(contact);
+    }
+    
+    public void replaceWithNewContact(int index, Contact contact){
+        for (int i = 0; i < contactList.size(); i++) {
+            if(i == index){
+                contactList.set(index, contact);
+            }
+        }
+    }
+    
+    public ArrayList<Contact> searchByCity(String city){
+        ArrayList<Contact> result = new ArrayList<>();
+        for (int i = 0; i < contactList.size(); i++) {
+            if(contactList.get(i).getHomeAddress().city.equals(city)){
+                result.add(contactList.get(i));
+               
+           }
+        }
+        return result;
+    }
+    
+    public Contact searchFirstNameAndLastName(String firstName, String lastName){
+        for (int i = 0; i < contactList.size(); i++) {
+            if(contactList.get(i).getFirstName().equals(firstName) && contactList.get(i).getLastName().equals(lastName) ){
+               return contactList.get(i);
+           }
+        }
+        return null;
+    }
+        
+        
+    public int getLength(){
+        return  contactList.size();
+    }
+    
+    
+    
+}
